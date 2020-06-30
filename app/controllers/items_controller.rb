3c8182933f4be9ae40adc21d3dd77a98040bc2fa
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-06-30 23:44:40 +0900

    新規投稿機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,28 @@
 class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all.order(created_at: :desc)
+  end
+
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path
+    else
+      render 'new'
+    end
   end
 
   def show
   end
+
+    private
+      def item_params
+        params.require(:item).permit(:title, :content, :price, :region)
+      end
+
 end
