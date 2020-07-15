d7b34f534b2521c06376a994f7a383797afe8509
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-07-16 01:01:03 +0900

    [ADD]パスワード再設定機能を追加
    close #29

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,64 @@
+class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "パスワード再設定メールを送信しました。"
+      redirect_to root_url
+    else
+      flash.now[:denger] = "入力されたEメールアドレスは登録されていません。"
+      render "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, :blank)
+      render 'edit'
+    elsif @user.update(user_params)
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    # beforeフィルタ
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # 有効なユーザーかどうか確認する
+    def valid_user
+      unless (@user && @user.activated? &&
+              @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
+
+    # トークンが期限切れかどうか確認する
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "パスワード再設定リンクの有効期限が切れてます。お手数ですが、もう一度やり直してください。"
+        redirect_to new_password_reset_url
+      end
+    end
+end
