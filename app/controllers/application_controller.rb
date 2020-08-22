aa4e61a9f7f703b3ba46ce6ace3879196a99f322
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-08-22 23:45:37 +0900

    マイページ、投稿ページ（新規、編集）にログイン制限を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  def authenticate_user
+    unless logged_in?
+      flash[:danger] = "ログインをして下さい。"
+      redirect_to login_url
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user, only: [:show, :edit, :update]
 
   def index
     @users = User.all
