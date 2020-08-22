c4acf20e96a92f870a40be2142fd312ad55ae5e6
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-08-22 23:52:08 +0900

    テストユーザーのパスワード確認の項目を削除
    ラベル名を変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,6 @@ FactoryBot.define do
     nickname  { "Alice" }
     sequence(:email) { |n| "exemple#{n}@example.com"}
     password { "password" }
-    password_confirmation { "password" }
     activated { true }
   end
 end
