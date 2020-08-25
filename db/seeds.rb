57218346cb92efac34d4b749769df19000f2947d
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-08-26 00:02:50 +0900

    seedデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,15 @@ User.create!(nickname: "Kiyosuke",
              password: "password",
              activated: true,
              admin: true)
+
+User.create!(nickname: "Kiyo2",
+             email: "kiyo2@yahoo.co.jp",
+             password: "password",
+             activated: true,
+             admin: true)
+
+User.create!(nickname: "Kiyo3",
+             email: "kiyo3@yahoo.co.jp",
+             password: "password",
+             activated: true,
+             admin: true)
