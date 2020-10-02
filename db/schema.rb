03eaa21aa33e4bbcacbeaa0986d732bd97880701
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-10-02 18:21:21 +0900

    introductionカラムをUsersテーブルに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_015518) do
+ActiveRecord::Schema.define(version: 2020_10_02_080738) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
