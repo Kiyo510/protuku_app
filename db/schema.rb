10a25ac99670b4757d7f0fc5041a1f57c484a195
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-10-02 18:18:50 +0900

    [Add]ユーザーが自己紹介文を登録する機能を追加

--- db/schema.rb
+++ db/schema.rb
@@ -129,6 +129,7 @@ ActiveRecord::Schema.define(version: 2020_09_07_015518) do
     t.datetime "reset_sent_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
