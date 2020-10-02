e3165f5dc245f44692f01018f7b5d5581683d69c
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-10-02 19:35:18 +0900

    自己紹介文のpresence trueを解除

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
                     uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  validates :introduction, presence: true, length: { maximum: 2000 }
+  validates :introduction, length: { maximum: 2000 }
 
   # 渡された文字列のハッシュ値を返す
   def self.digest(string)
