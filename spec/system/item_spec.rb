09fffc31da1da52643f98690744cb8225bcfcdee
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-10-04 21:26:00 +0900

    item_pathの引数をuserに紐づくitemオブジェクトに変更

--- spec/system/item_spec.rb
+++ spec/system/item_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.feature '投稿機能', type: :system do
   let(:user) { FactoryBot.create(:user) }
   let(:other_user) { FactoryBot.create(:other_user) }
+  let!(:item) { FactoryBot.create(:item, content: 'first post', user_id: user.id) }
   describe "投稿一覧表示機能" do
     before do
-      FactoryBot.create(:item, content: 'first post', user_id: user.id)
       visit login_path
       fill_in 'session[email]', with: login_user.email
       fill_in 'session[password]', with: login_user.password
@@ -23,7 +23,7 @@ RSpec.feature '投稿機能', type: :system do
     context "投稿を削除したとき" do
       let(:login_user) { user }
       it "投稿の削除に成功すること" do
-        visit item_path(user)
+        visit item_path(item)
         page.accept_confirm do
           click_link '削除'
         end
@@ -39,13 +39,13 @@ RSpec.feature '投稿機能', type: :system do
       end
 
       it '投稿詳細ページに編集、削除ボタンが表示される' do
-        visit item_path(user)
+        visit item_path(item)
         expect(page).to have_content '編集'
         expect(page).to have_content '削除'
       end
 
       it "投稿詳細ページにメッセージを送るボタンが表示されない" do
-        visit item_path(user)
+        visit item_path(item)
         expect(page).not_to have_css '.message_btn'
       end
     end
@@ -57,13 +57,13 @@ RSpec.feature '投稿機能', type: :system do
       end
 
       it '投稿詳細ページに編集、削除ボタンが表示されない' do
-        visit item_path(user)
+        visit item_path(item)
         expect(page).not_to have_content '編集'
         expect(page).not_to have_content '削除'
       end
 
       it "投稿詳細ページにメッセージを送るボタンが表示さる" do
-        visit item_path(user)
+        visit item_path(item)
         expect(page).to  have_css '.message_btn'
       end
     end
