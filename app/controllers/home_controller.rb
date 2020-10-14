b4a59e42c21014ee3423e7e06683cb51f5578908
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-10-14 12:52:01 +0900

    TOPに表示される最近の募集の件数を20件に変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   before_action :forbid_login_user
 
   def home
-    @items = Item.includes(:user).order('created_at DESC').limit(15)
+    @items = Item.includes(:user).order('created_at DESC').limit(20)
   end
 end
