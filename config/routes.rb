a0f13588d265b77f2647688df9bc078a769d11c7
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-10-18 02:09:50 +0900

    不要なルーティングを削除、修正

--- config/routes.rb
+++ config/routes.rb
@@ -9,24 +9,11 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users, expect: :index
-  resources :items do
-    collection do
-      get  'purchase/:id',to: 'items#purchase', as: 'purchase'
-      post 'pay/:id',to:  'items#pay', as: 'pay'
-      get  'done', to:    'items#done', as: 'done'
-    end
-  end
-  resources :messages, only: [:create, :destroy]
-  resources :rooms, only: [:show, :create, :index]
+  resources :items
+  resources :messages, only: %i(create destroy)
+  resources :rooms, only: %i(show create index)
   resources :stocks, only: %i(index create destroy)
-  resources :account_activations, only: [:edit]
-  resources :notifications, only: [:index]
-  resources :password_resets,     only: [:new, :edit, :create, :update]
-  resources :cards, only: [:new, :show] do
-    collection do
-    post 'show', to: 'cards#show'
-    post 'pay', to: 'cards#pay'
-    post 'delete', to: 'cards#delete'
-    end
-  end
+  resources :account_activations, only: %i(edit)
+  resources :notifications, only: %i(index)
+  resources :password_resets, only: %i(new edit create update)
 end
