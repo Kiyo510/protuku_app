be313ebf863abd14249ac41c8d9c1e26bb7895c0
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-10-18 14:24:51 +0900

    DM送信時の文字数を2000文字までにした

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 2000 }
   belongs_to :user
   belongs_to :room
   has_many :notifications, dependent: :destroy
