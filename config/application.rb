24acb0132e0f467b60d2d233d88df4f23033bc9f
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-10-18 14:31:26 +0900

    エラーメッセージを日本語化した

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Webapp
     end
     config.generators.system_tests = nil
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
   end
 end
