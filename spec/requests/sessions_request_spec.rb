15e2ad8ad50b38c71909ce77fe7a3a998be2672b
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-10-21 00:18:07 +0900

    ログインページのテストを追記

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Sessions', type: :request do
   describe 'GET /new' do
-    it 'returns http success' do
-      get '/sessions/new'
+    it 'アクセスに成功すること' do
+      get login_path
       expect(response).to have_http_status(:success)
     end
   end
