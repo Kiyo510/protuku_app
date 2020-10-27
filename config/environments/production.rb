ec70d00dcad36d47acdb72d2c4b060f5d28b9c61
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-10-27 23:04:16 +0900

    本番環境でのメイラーの設定を追記

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,11 +64,11 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "protuku.com" }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
-
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
