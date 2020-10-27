01333c374c46a9858fb58b53308677f31cee6e65
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-10-27 22:54:07 +0900

    [fix] Net::SMTPAuthenticationError:を修正

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,10 +1,13 @@
+gmail_user_name = ENV['GMAIL_USER_NAME']
+gmail_app_password = ENV['GMAIL_APP_PASSWORD']
+
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   address: 'smtp.gmail.com',
   domain: 'protuku.com',
   port: 587,
-  user_name: ENV['GMAIL_USER_NAME'],
-  password: ENV['GMAIL_APP_PASSWORD'],
+  user_name: gmail_user_name,
+  password: gmail_app_password,
   authentication: :login,
   enable_starttls_auto: true
 }
