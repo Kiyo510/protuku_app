3097af3ad6992f89b11e7dfbd97573a673458627
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-10-28 21:51:29 +0900

    gmailの設定を追記、変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,16 +68,16 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'protuku.com',:port => 80 }
+  config.action_mailer.default_url_options = { :host => 'protuku.com' }
   config.action_mailer.smtp_settings = {
-  address: 'smtp.gmail.com',
-  domain: 'gmail.com',
-  port: 587,
-  user_name: ENV['GMAIL_USER_NAME'],
-  password: ENV['GMAIL_APP_PASSWORD'],
-  authentication: :login,
-  enable_starttls_auto: true
-}
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    user_name: ENV['GMAIL_USER_NAME'],
+    password: ENV['GMAIL_APP_PASSWORD'],
+    authentication: :login,
+    enable_starttls_auto: true
+  }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
