d61dcb46848f496732d51b0a14fcd9669ee96ed4
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-10-29 00:31:22 +0900

    mailerの設定を修正

--- config/environment.rb
+++ config/environment.rb
@@ -1,18 +1,18 @@
 # Load the Rails application.
 require_relative 'application'
 # Initialize the Rails application.
-Rails.application.configure do
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'protuku.com' }
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    domain: 'smtp.gmail.com',
-    port: 587,
-    user_name: ENV['GMAIL_USER_NAME'],
-    password: ENV['GMAIL_APP_PASSWORD'],
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
-end
+# Rails.application.configure do
+#   config.action_mailer.raise_delivery_errors = true
+#   config.action_mailer.delivery_method = :smtp
+#   config.action_mailer.default_url_options = { :host => 'protuku.com' }
+#   config.action_mailer.smtp_settings = {
+#     address: 'smtp.gmail.com',
+#     domain: 'smtp.gmail.com',
+#     port: 587,
+#     user_name: ENV['GMAIL_USER_NAME'],
+#     password: ENV['GMAIL_APP_PASSWORD'],
+#     authentication: :plain,
+#     enable_starttls_auto: true
+#   }
+# end
 Rails.application.initialize!
