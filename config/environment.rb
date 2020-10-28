80f2c667efeb64ef2ba20310a779f317f69e5b30
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-10-28 16:04:24 +0900

    gmailの設定を追記

--- config/environment.rb
+++ config/environment.rb
@@ -2,12 +2,3 @@
 require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
-config.action_mailer.smtp_settings = {
-  address: 'smtp.gmail.com',
-  domain: 'gmail.com',
-  port: 587,
-  user_name: ENV['GMAIL_USER_NAME'],
-  password: ENV['GMAIL_APP_PASSWORD'],
-  authentication: :login,
-  enable_starttls_auto: true
-}
