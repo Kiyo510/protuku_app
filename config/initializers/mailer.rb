b0d1b32bc44c579cfdb27d2705a59a883c8333a2
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-10-29 01:15:29 +0900

    :domainを元に戻した

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   address: 'smtp.gmail.com',
-  domain: 'protuku.com',
+  domain: 'gmail.com',
   port: 587,
   user_name: 'miraishida00510@gmail.com',
   password: ENV['GMAIL_APP_PASSWORD'],
