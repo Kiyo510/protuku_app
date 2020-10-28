0b0604bbc98950b932f52b653fb48330d0ab53d5
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-10-28 22:21:02 +0900

    mailerの設定を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,9 +66,8 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'protuku.com' }
+  config.action_mailer.default_url_options = { :host => 'protuku.com', port: 80 }
   gmail_user_name = ENV['GMAIL_USER_NAME']
   gmail_app_password = ENV['GMAIL_APP_PASSWORD']
   config.action_mailer.smtp_settings = {
