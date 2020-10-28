1099ea83c84a5923db23f90a7fad2d7097ed4aa4
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-10-28 13:06:30 +0900

    [fix]gmailの認証方式をplainに変更

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -4,10 +4,10 @@ gmail_app_password = ENV['GMAIL_APP_PASSWORD']
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   address: 'smtp.gmail.com',
-  domain: 'protuku.com',
+  domain: 'gmail.com',
   port: 587,
   user_name: gmail_user_name,
   password: gmail_app_password,
-  authentication: :login,
+  authentication: 'plain',
   enable_starttls_auto: true
 }
