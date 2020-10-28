b5abf85dda975122aef92ed146b2172cd0de969e
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-10-28 14:52:15 +0900

    gmailの自動送信メールの設定をenvironment.rbに移行

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,18 @@
 # Load the Rails application.
 require_relative 'application'
-
 # Initialize the Rails application.
 Rails.application.initialize!
+
+gmail_user_name = ENV['GMAIL_USER_NAME']
+gmail_app_password = ENV['GMAIL_APP_PASSWORD']
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.gmail.com',
+  domain: 'gmail.com',
+  port: 587,
+  user_name: gmail_user_name,
+  password: gmail_app_password,
+  authentication: :login,
+  enable_starttls_auto: true
+}
--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,13 +1,13 @@
-gmail_user_name = ENV['GMAIL_USER_NAME']
-gmail_app_password = ENV['GMAIL_APP_PASSWORD']
+# gmail_user_name = ENV['GMAIL_USER_NAME']
+# gmail_app_password = ENV['GMAIL_APP_PASSWORD']
 
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  address: 'smtp.gmail.com',
-  domain: 'gmail.com',
-  port: 587,
-  user_name: gmail_user_name,
-  password: gmail_app_password,
-  authentication: :login,
-  enable_starttls_auto: true
-}
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = {
+#   address: 'smtp.gmail.com',
+#   domain: 'gmail.com',
+#   port: 587,
+#   user_name: gmail_user_name,
+#   password: gmail_app_password,
+#   authentication: :login,
+#   enable_starttls_auto: true
+# }
