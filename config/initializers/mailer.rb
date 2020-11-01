463620c29e7bae8e10a4dad206aca4a453437a09
Kiyo510 <62272140+Kiyo510@users.noreply.github.com>
2020-11-02 02:20:47 +0900

    環境変数を修正

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.gmail.com',
   domain: 'gmail.com',
   port: 587,
-  user_name: ENV['GMAIL_USER_NAME'],
-  password: ENV['GMAIL_APP_PASSWORD'],
+  user_name: 'miraishida00510@gmail.com',
+  password: 'ogqlbhuplejgshuz',
   authentication: :login,
   enable_starttls_auto: true
 }
