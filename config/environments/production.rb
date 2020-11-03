2adad3cd451d388f7783ef3148c0a04c0f9b302b
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-11-04 00:23:09 +0900

    ホストを設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  config.hosts << IPAddr.new('10.0.0.0/16')
+  config.hosts << "protuku.com"
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "webapp_production"
