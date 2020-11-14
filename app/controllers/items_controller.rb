c55686a80b10de68253af06c41d57ebdb3c39f79
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-11-14 23:17:10 +0900

    return if or unlessを用いてコードをリファクタリングした

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,12 +39,12 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    if logged_in?
-      @current_user_entry = Entry.where(user_id: current_user.id)
-      # Entryモデルからメッセージ相手のレコードを抽出
-      @another_user_entry = Entry.where(user_id: @item.user_id)
-      set_room_for_direct_message unless @item.user_id == current_user.id
-    end
+    return unless logged_in?
+
+    @current_user_entry = Entry.where(user_id: current_user.id)
+    # Entryモデルからメッセージ相手のレコードを抽出
+    @another_user_entry = Entry.where(user_id: @item.user_id)
+    set_room_for_direct_message unless @item.user_id == current_user.id
   end
 
   def edit
--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -56,9 +56,9 @@ class PasswordResetsController < ApplicationController
 
   # トークンが期限切れかどうか確認する
   def check_expiration
-    if @user.password_reset_expired?
-      flash[:danger] = 'パスワード再設定リンクの有効期限が切れてます。お手数ですが、もう一度やり直してください。'
-      redirect_to new_password_reset_url
-    end
+    return unless @user.password_reset_expired?
+
+    flash[:danger] = 'パスワード再設定リンクの有効期限が切れてます。お手数ですが、もう一度やり直してください。'
+    redirect_to new_password_reset_url
   end
 end
