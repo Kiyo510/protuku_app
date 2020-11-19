ffa588ecac3e2a78d606ce7a28f4550ad140d849
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-11-19 21:28:07 +0900

    インデントを修正

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,14 +8,14 @@ class Message < ApplicationRecord
 
   def create_notification_message!(another_user, current_user)
     notification = current_user.active_notifications.build(
-        room_id: room_id,
-        message_id: id,
-        visited_id: another_user.user_id,
-        visitor_id: current_user.id,
-        action: 'dm'
-      )
-      # 自分に対するメッセージの場合、通知はこないようにする。
-      notification.checked = true if notification.visitor_id == notification.visited_id
-      notification.save if notification.valid?
+      room_id: room_id,
+      message_id: id,
+      visited_id: another_user.user_id,
+      visitor_id: current_user.id,
+      action: 'dm'
+    )
+    # 自分に対するメッセージの場合、通知はこないようにする。
+    notification.checked = true if notification.visitor_id == notification.visited_id
+    notification.save if notification.valid?
   end
 end
