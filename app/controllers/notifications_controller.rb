99f2dd8f937fc874a23b5463e57133555540292d
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-11-23 01:54:00 +0900

    eager_loadなどを使い、発行するSQLの回数を減らした

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,8 +2,8 @@
 
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.passive_notifications
-    @notifications.where(checked: false).each do |notification|
+    @notifications = current_user.passive_notifications.eager_load(:visitor)
+    @notifications.where(checked: false).find_each do |notification|
       notification.update(checked: true)
     end
   end
