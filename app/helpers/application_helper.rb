f2d307cf40a11e5d6e27f5809eba04f23c78702b
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-11-25 23:06:50 +0900

    配列やハッシュの最後にあるカンマを削除した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,10 +12,10 @@ module ApplicationHelper
         'プロつく！はプログラミング仲間を探している人同士を結びつけるマッチングサービスです。「プログラミングの独学で挫折しそう」、
         「プログラミングスクールにいきたいけどお金がない」と悩んでいるそこのあなた！プロつくで仲間を集め、チームで支え合う環境を作ってみませんか？',
       canonical: request.original_url,
-      noindex: ! Rails.env.production?,
+      noindex: !Rails.env.production?,
       icon: [
         { href: image_url('protuku_favicon.png') },
-        { href: image_url('protuku_favicon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
+        { href: image_url('protuku_favicon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' }
       ],
       og: {
         site_name: 'プロつく！',
@@ -26,11 +26,11 @@ module ApplicationHelper
         type: 'website',
         url: request.original_url,
         image: image_url('protuku_ogp.png'),
-        locale: 'ja_JP',
+        locale: 'ja_JP'
       },
       twitter: {
         card: 'summary_large_image',
-        site: '@Kiyo_Karl2',
+        site: '@Kiyo_Karl2'
       }
     }
   end
