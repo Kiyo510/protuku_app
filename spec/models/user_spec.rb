527b21d73ff7ec9165863e154872d2728c3df88f
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2020-11-27 16:58:04 +0900

    自己紹介の文字数制限を2000文字から200文字へ修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_length_of(:email).is_at_most(255) }
   it { is_expected.to validate_presence_of :password }
   it { is_expected.to validate_length_of(:password).is_at_least(6) }
-  it { is_expected.to validate_length_of(:introduction).is_at_most(2000) }
+  it { is_expected.to validate_length_of(:introduction).is_at_most(200) }
   it { should validate_acceptance_of(:accepted).with_message('をチェックしてください') }
 
   describe 'メールアドレスの有効性' do
